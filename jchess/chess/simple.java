d50f57270122800dfa4b5e5b8509aa18ac0078d8
as-is <simonov.ant@gmail.com>
2017-11-30 18:02:46 +0300

    dev ::  + chess project

---
+++ jchess/chess/simple.java
@@ -0,0 +1,13 @@
+// $Id: $
+// Author: Anton Simonov
+
+package chess;
+
+public class simple
+{
+	// -----------------------------------------------------------------------------------------------------------------
+	public static void main( String[] args )
+	{
+		System.out.println("www");
+	}
+}
