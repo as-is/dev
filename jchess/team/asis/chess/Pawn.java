656c2be3486439fce9dfd2039190d11810db45f6
as-is <simonov.ant@gmail.com>
2017-12-02 14:20:18 +0300

    d

---
+++ jchess/team/asis/chess/Pawn.java
@@ -0,0 +1,14 @@
+package team.asis.chess;
+
+import static team.asis.chess.Utils.*;
+
+public class Pawn
+{
+	private String place = "e2";
+
+	public Pawn( String place )
+	{
+		MUST(true, "");
+		//place.length()
+	}
+}
